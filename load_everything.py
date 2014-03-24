78b36bb271512c1ad795ce5feaf5223caff7e3fa
Maarten Derickx <m.derickx.student@gmail.com>
2014-03-24 18:13:20 +0100

    Added doctest and documentation to load_everything.py

--- load_everything.py
+++ load_everything.py
@@ -1,3 +1,12 @@
+"""
+This file contains commands that load every function in this project.
+Usage is as follows
+
+EXAMPLE::
+
+  sage: load("https://raw.github.com/koffie/mdsage/master/load_everything.py")
+
+"""
 load("https://raw.github.com/koffie/mdsage/master/maartens_sage_functions.py")
 load("https://raw.github.com/koffie/mdsage/master/modular_unit_divisors.py")
 load("https://raw.github.com/koffie/mdsage/master/cuspidal_classgroup.py")
