5edf07ad013fa24475eaae80b7739f30c5ed9b74
Maarten Derickx <m.derickx.student@gmail.com>
2014-03-24 18:02:23 +0100

    Update load_everything.py
    Added "cuspidal_classgroup.py" to load everything.

--- load_everything.py
+++ load_everything.py
@@ -1,2 +1,3 @@
 load("https://raw.github.com/koffie/mdsage/master/maartens_sage_functions.py")
 load("https://raw.github.com/koffie/mdsage/master/modular_unit_divisors.py")
+load("https://raw.github.com/koffie/mdsage/master/cuspidal_classgroup.py")
