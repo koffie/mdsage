18fdefa72dde42bafb697f35456fcd2b5271d676
Maarten Derickx <m.derickx.student@gmail.com>
2014-05-19 11:01:42 +0200

    move a function to a different file

--- cuspidal_classgroup.py
+++ cuspidal_classgroup.py
@@ -16,3 +16,39 @@ def intersection(self,other):
     """
     assert self.W()==other.W()
     return self.V().intersection(other.V()).quotient(self.W())
+
+
+def cuspidal_rational_subgroup_mod_rational_cuspidal_subgroup(G):
+    """On input a congruence subgroup G that contains Gamma1(N) checks whether there are cuspsums that are not equivalent to a cuspsum defined over Q but that are equivalent to a divisor defined over Q.
+    
+    INPUT:
+    
+    - G - a congruence subgroup
+    
+    OUTPUT:
+    
+    - the invariants of the cuspidal rational subgroup divided out by the rational cuspidal subgroup 
+    
+    EXAMPLES::
+        
+        sage: cuspidal_rational_subgroup_mod_rational_cuspidal_subgroup(Gamma1(26))
+        ()
+    """
+    if G.genus() == 0:
+        return tuple()
+        
+    N=G.level()
+    ZZcusp=ZZ**G.ncusps()
+    unit_gens=Integers(N).unit_gens()
+    L,D=modular_unit_lattice(G)
+    Lrat,Drat=rational_modular_unit_lattice(G)
+    DmodL=D.quotient(L)
+    DmodLrat=Drat.quotient(Lrat)
+    kernels=[]
+    for g in unit_gens:
+        m=Matrix([ZZcusp.gen(G.cusps().index(G.reduce_cusp(galois_action(c,g,N)))) for c in G.cusps() ])
+        f = DmodL.hom([DmodL(i.lift()*(m-1)) for i in  DmodL.gens()])
+        kernels.append(f.kernel())
+        #print kernels[-1]
+    rat_cusp_tors=reduce(intersection,kernels)
+    return rat_cusp_tors.V().quotient(Drat+L).invariants()
